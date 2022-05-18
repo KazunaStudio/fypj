757de32cb848aac4428aa26c2cd81e4f4f1eb31b
Rianne Ong <RozeliaDiamondair@gmail.com>
2022-05-18 13:03:58 +0800

    Updates

--- src/Modules/Variables.js
+++
@@ -1,6 +0,0 @@
-// To import the data from retrieve function in VS C# API
-// Do not touch this variables unless the numbers are different
-export const variables={
-    API_URL:"http://localhost:34140/api/",
-    IMG_URL:"http://localhost:34140/Images/"
-}
